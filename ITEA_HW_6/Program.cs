fd3dd4d7460995659ee40c4a4b46abf9c41f1b64
Рената Цікалова <ts.renata98@gmail.com>
2021-12-29 17:30:10 +0200

    Add project files.

---
+++ ITEA_HW_6/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ITEA_HW_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
